b72a0ffa5a10fef0b16b772a21ff3dc0b8f0b4d4
flowriance <16399170+flowriance@users.noreply.github.com>
2018-06-19 14:07:24 +0200

    Update MineSweeper.asm

--- MineSweeper.asm
+++ MineSweeper.asm
@@ -1,93 +1,130 @@
-BEGIN:	ORG 00H
-	MOV DPTR,#RANDOM_NUMBER ;// moves starting address of LUT to DPTR
-	MOV A,#11111111B ;// loads A with all 1's
-	MOV P0,#00000000B ;// initializes P0 as output port
-	MOV R2,#4D
+;--------------------Minigame--------------------
+;
+; Course: 	Systemname Programmierung
+; Author:	Denny Flämig & Florian Christof
+;
+	ORG 00H
+BEGIN:	MOV A,#11111111B 	;// loads A with all 1's
+	MOV P0,#00000011B 	;// initializes P0 as output port
+	MOV R2,#4D		;// initialize number of free minefields + 1
 
-BACK:	INC R2
-	MOV P1,#11111111B ;// loads P1 with all 1's
-     	CLR P1.0  ;// makes row 1 low
-     	JB P1.4,NEXT1  ;// checks whether column 1 is low and jumps to NEXT1 if not low
-     	MOV R0,#00000000B   ;// loads a with 0B if column is low (that means key 1 is pressed)
-     	ACALL CHECK  ;// calls DISPLAY subroutine
-NEXT1:	JB P1.5,NEXT2 ;// checks whether column 2 is low and so on...
-     	MOV R0,#00000001B  
+; -----------------
+; Polling of Matrix-Keypad
+;------------------
+BACK:	MOV DPTR,#RANDOM_NUMBER ;// moves starting address of RANDOM_NUMBER to DPTR
+	MOV P1,#11111111B 	;// loads P1 with all 1's
+     	CLR P1.0  		;// makes row 1 low
+     	JB P1.4,NEXT1  		;// checks whether column 1 is low and jumps to NEXT1 if not low
+     	MOV R0,#00000000B   	;// loads a with 0B if column is low (that means key 1 is pressed)
+     	ACALL CHECK  		;// calls CHECK subroutine
+NEXT1:	INC DPTR
+	JB P1.5,NEXT2 		;// checks whether column 2 is low and so on...
+     	MOV R0,#00000001B
       	ACALL CHECK
-NEXT2:	JB P1.6,NEXT3 
-     	MOV R0,#00000010B  
+NEXT2:	INC DPTR
+	JB P1.6,NEXT3
+	MOV R0,#00000010B
       	ACALL CHECK
-NEXT3:	JB P1.7,NEXT4 
-     	MOV R0,#00000011B  
+NEXT3:	INC DPTR
+	JB P1.7,NEXT4
+     	MOV R0,#00000011B
       	ACALL CHECK
-NEXT4:	SETB P1.0
+NEXT4:	INC DPTR
+	SETB P1.0
       	CLR P1.1
       	JB P1.4,NEXT5
-     	MOV R0,#00000100B  
+     	MOV R0,#00000100B
       	ACALL CHECK
-NEXT5:	JB P1.5,NEXT6
-     	MOV R0,#00000101B  
+NEXT5:	INC DPTR
+	JB P1.5,NEXT6
+     	MOV R0,#00000101B
       	ACALL CHECK
-NEXT6:	JB P1.6,NEXT7
-     	MOV R0,#00000110B  
+NEXT6:	INC DPTR
+	JB P1.6,NEXT7
+     	MOV R0,#00000110B
       	ACALL CHECK
 NEXT7:	JB P1.7,NEXT8
-     	MOV R0,#00000111B  
+     	MOV R0,#00000111B
       	ACALL CHECK
-NEXT8:	SETB P1.1
+NEXT8:	INC DPTR
+	SETB P1.1
       	CLR P1.2
       	JB P1.4,NEXT9
      	MOV R0,#00001000B
       	ACALL CHECK
-NEXT9:	JB P1.5,NEXT10
+NEXT9:	INC DPTR
+	JB P1.5,NEXT10
      	MOV R0,#00001001B
       	ACALL CHECK
-NEXT10:	JB P1.6,NEXT11
+NEXT10:	INC DPTR
+	JB P1.6,NEXT11
      	MOV R0,#00001010B
        	ACALL CHECK
-NEXT11:	JB P1.7,NEXT12
+NEXT11:	INC DPTR
+	JB P1.7,NEXT12
      	MOV R0,#00001011B
        	ACALL CHECK
-NEXT12:	SETB P1.2
+NEXT12:	INC DPTR
+	SETB P1.2
        	CLR P1.3
        	JB P1.4,NEXT13
      	MOV R0,#00001100B
        	ACALL CHECK
-NEXT13:	JB P1.5,NEXT14
+NEXT13:	INC DPTR
+	JB P1.5,NEXT14
      	MOV R0,#00001101B
        	ACALL CHECK
-NEXT14:	JB P1.6,NEXT15
+NEXT14:	INC DPTR
+	JB P1.6,NEXT15
      	MOV R0,#00001110B
        	ACALL CHECK
-NEXT15:	JB P1.7,BACK
+NEXT15:	INC DPTR
+	JB P1.7,BACK_JUMP
     	MOV R0,#00001111B
        	ACALL CHECK
        	LJMP BACK
 
-INTERMEDIATE_BACK: JMP BACK  ;//Sprung, da JNZ nicht weit genug springen kann
+; -----------------
+; Jump-Points
+;------------------
+BEGIN_JUMP:	JMP BEGIN
+BACK_JUMP:	JMP BACK
+; -----------------
+; Gamelogic
+;------------------
 
-CHECK:	MOV A,R2
+
+CHECK:	JB P1.4, CHECK3
+	JMP CHECK
+CHECK3:	JB P1.5, CHECK4
+	JMP CHECK3
+CHECK4: JB P1.6, CHECK5
+	JMP CHECK4
+CHECK5:	JB P1.7, CHECK2
+	JMP CHECK5
+
+CHECK2:	MOV A,R2
 	JNZ CHECK1
 	JMP WIN
-CHECK1:	MOV A,R0
-	MOVC A,@A+DPTR
-	JNZ INTERMEDIATE_BACK
-	MOV A,R0	;//Gedrücktes Feld zur Bombe machen, damit der 
-	JMP @A+DPTR	;//Counter nicht ausgetrickst werden kann.
-	MOV A, #00000001B
-	MOVX @DPTR,A
-	JMP BOMB
-BOMB:	MOV P0,#10001000B
-	;MOV A,#11111111B
-	CLR P1.3
-LOOP:	JB P1.7, LOOP
-	MOV DPTR,#RANDOM_NUMBER ;// moves starting address of LUT to DPTR
-	MOV A,#11111111B ;// loads A with all 1's
-	MOV P0,#00000000B ;// initializes P0 as output port
-	MOV R2,#4D
-	JMP BACK
-WIN:	MOV P0,#00010001B
-	;MOV A,#11111111B
-	JMP LOOP
+
+WIN:	CLR P0.1
+	JMP RESTARTINIT
+
+CHECK1:	MOVX A,@DPTR
+	JNZ BOMB
+	JMP NOBOMB
+
+BOMB: 	CLR P0.0
+	JMP RESTARTINIT
+NOBOMB:	MOV A, #01B
+	MOVX @DPTR, A
+	DEC R2
+	JMP BACK_JUMP
+
+RESTARTINIT:	MOV P1,#11111111B 	;// loads P1 with all 1's
+		CLR P1.3  		;// makes row 1 low
+RESTART:	JB P1.7,RESTART
+		JMP BEGIN_JUMP
 
 
 RANDOM_NUMBER:	DB 0B ;// Look up table starts here
